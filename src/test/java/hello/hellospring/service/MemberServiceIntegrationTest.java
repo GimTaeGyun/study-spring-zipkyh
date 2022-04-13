68bceb9244c14c7f4a3d434e08f10df34aa83552
gtg <xorbsdut@gmail.com>
2022-04-13 22:16:13 +0900

    Feat: 스프링 통합 테스트까지 from NoteBook: close #1

---
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,55 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+
+        /*
+            hello라는 id로 가입하고 반환된 id를 통해 다시 회원을 찾아서
+            new Member객체의 name과 가입된 Member의 name을 비교한다.
+         */
+
+        //given
+        Member member = new Member();
+        member.setName("태균1");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+    }
+
+}
